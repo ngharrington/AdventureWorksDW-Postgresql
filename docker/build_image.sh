b8b8cb6fb415f50843497912835d68308fe3d2df
Neal Harrington <ngharrington@users.noreply.github.com>
2023-01-22 23:00:47 -0500

    Add sql server image build

---
+++ docker/build_image.sh
@@ -0,0 +1,13 @@
+set -e
+
+# Run this from the root of this repo.
+# i.e. ./docker/build_image.sh
+
+# download AdventureWorks DW dababase backup and build a sql server docker image
+# that has this backup restored to it.
+
+echo "Downloading Adventureworks2019DW Data Warehouse backup..."
+wget -O ./docker/AdventureWorksDW2019.bak -q https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorksDW2019.bak
+docker build --no-cache ./docker/ -t pyreql-test-sqlserver -f ./docker/sqlserver.dockerfile
+rm ./docker/AdventureWorksDW2019.bak
+
